a190d1667ec2d6cea5052f16d6ee8f305e5848b2
Harold <power0721@gmail.com>
2023-01-08 10:47:33 +0800

    Support IDEA 2022.3

--- src/main/kotlin/cn/har01d/plugin/kitty/services/KittyPreloadingActivity.kt
+++ src/main/kotlin/cn/har01d/plugin/kitty/services/KittyPreloadingActivity.kt
@@ -2,10 +2,9 @@ package cn.har01d.plugin.kitty.services
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PreloadingActivity
-import com.intellij.openapi.progress.ProgressIndicator
 
 class KittyPreloadingActivity : PreloadingActivity() {
-    override fun preload(indicator: ProgressIndicator) {
+    override suspend fun execute() {
         ApplicationManager.getApplication().getService(KittyApplicationService::class.java)
     }
 }
