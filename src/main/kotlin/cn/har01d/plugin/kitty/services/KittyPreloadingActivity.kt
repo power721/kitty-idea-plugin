79765a1abdd4454ab35e59bb5f30b4c77c7826d6
Har01d <power0721@gmail.com>
2024-10-12 17:26:50 +0800

    Continue work 5 minutes

--- src/main/kotlin/cn/har01d/plugin/kitty/services/KittyPreloadingActivity.kt
+++ src/main/kotlin/cn/har01d/plugin/kitty/services/KittyPreloadingActivity.kt
@@ -1,10 +1,11 @@
 package cn.har01d.plugin.kitty.services
 
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
 
-class KittyPreloadingActivity : PreloadingActivity() {
-    override suspend fun execute() {
+class KittyPreloadingActivity : StartupActivity {
+    override fun runActivity(project: Project) {
         ApplicationManager.getApplication().getService(KittyApplicationService::class.java)
     }
 }
